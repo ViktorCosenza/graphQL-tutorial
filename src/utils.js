f96642d5abd2e9cbcbfa7ecc25aec6ea43202acc
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2019-08-04 12:17:16 -0300

    posts and login

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,11 @@
 import jwt from 'jsonwebtoken'
 
-const APP_SECRET = 'pipi'
+export const APP_SECRET = 'pipi'
 
-function getUserId (context) {
+export function getUserId (context) {
   const Authorization = context.request.get('Authorization')
   if (!Authorization) { throw new Error('Not Authenticated') }
   const token = Authorization.replace('Bearer ', '')
   const { userId } = jwt.verify(token, APP_SECRET)
   return userId
 }
-
-export default { APP_SECRET, getUserId }
